888b3b6ca8d5f46d483c9b8f77b5dd8a1e1d69ac
Seung Bin Hong <tmdqls2257@gmail.com>
2022-01-24 21:11:06 +0900

    Update_internship's_headers

--- server/controllers/page/internship.js
+++ server/controllers/page/internship.js
@@ -4,14 +4,14 @@ const axios = require('axios')
 const jobType = '11'
 const requestUrl = `${url}access-key=${process.env.INTETNSHIP_ACCESSKEY}&job_type=11`
 
+
 module.exports = async (req, res) => {
-  axios.get(requestUrl).then(data => {
-    if (!data.data.jobs.job) {
-      return res.status(400).json({ data: '', message: '인턴쉽 정보가 없습니다.' })
-    }
-    console.log(data.data.jobs.job)
-    return res.status(200).json({ data: data.data.jobs.job, message: 'oks' })
-  }).catch(err => {
-    return res.status(500).send(err)
-  })
+    axios.get(requestUrl,{headers:{'Accept':'application/json'}}).then(data => {
+        if(!data.data.jobs.job){
+            return res.status(400).json({data:'',message:'인턴쉽 정보가 없습니다.'})
+        }
+        return res.status(200).json({data: data.data.jobs.job, message:'oks'})
+    }).catch(err=>{
+        return res.status(500).send(err)
+    })
 }
