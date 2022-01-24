1f2baa903877a9f15af00286d7c0e7372617eaf8
shren207 <shren0812@gmail.com>
2022-01-24 11:23:12 +0900

    Create: Searchbar.js 컴포넌트 완성

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import Begin from './pages/Begin'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Begin />
-    {/* <App /> */}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
